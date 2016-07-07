2dc244fef9b609a08637428de8462bf5f79a5ae7
Michael Remijan <mjremijan@yahoo.com>
2016-07-06 19:36:12 -0500

    Highly voted way to read and execute sql from a file. If this doesnt' work I think I may go to a simple XML document format.

--- src/main/java/org/ferris/riviera/console/connection/ConnectionHandler.java
+++ src/main/java/org/ferris/riviera/console/connection/ConnectionHandler.java
@@ -1,8 +1,11 @@
 package org.ferris.riviera.console.connection;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 import javax.inject.Inject;
 import org.ferris.riviera.console.messages.Key;
 
@@ -15,17 +18,16 @@ public class ConnectionHandler {
     private Connection conn;
 
     @Inject
-    @Key(value="url", required=true)
+    @Key(value = "url", required = true)
     protected String url;
 
     @Inject
-    @Key(value="username", required=true)
+    @Key(value = "username", required = true)
     protected String username;
 
     @Inject
-    @Key(value="password", required=true)
+    @Key(value = "password", required = true)
     protected String password;
-
 
     public String getUrl() {
         return url;
@@ -48,5 +50,37 @@ public class ConnectionHandler {
             }
         }
         return conn;
+    }
+
+    /**
+     * http://stackoverflow.com/questions/1497569/how-to-execute-sql-script-file-using-jdbc
+     *
+     * @param conn
+     * @param in
+     * @throws SQLException
+     */
+    public static void importSQL(Connection conn, InputStream in) throws SQLException {
+        Scanner s = new Scanner(in);
+        //s.useDelimiter("(;(\r)?\n)|(--\n)");
+        s.useDelimiter("(;(\r)?\n)|((\r)?\n)?(--)?.*(--(\r)?\n)");
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            while (s.hasNext()) {
+                String line = s.next();
+                if (line.startsWith("/*!") && line.endsWith("*/")) {
+                    int i = line.indexOf(' ');
+                    line = line.substring(i + 1, line.length() - " */".length());
+                }
+
+                if (line.trim().length() > 0) {
+                    st.execute(line);
+                }
+            }
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
     }
 }
