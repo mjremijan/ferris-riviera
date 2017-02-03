7f525dbbf65b2ebaf538566925c40ffae161b1b9
Michael Remijan <mjremijan@yahoo.com>
2017-02-02 21:07:45 -0600

    RIVIERA-12: Did some testing. looks like getComplement() works great. finished up {{JarEntryFilter}}

--- riviera-console/src/main/java/org/ferris/riviera/console/jar/JarEntryFilter.java
+++ riviera-console/src/main/java/org/ferris/riviera/console/jar/JarEntryFilter.java
@@ -3,6 +3,7 @@ package org.ferris.riviera.console.jar;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import org.apache.log4j.Logger;
+import static org.ferris.riviera.console.jar.JarEntryFinderEvent.FILTER;
 import org.jboss.weld.experimental.Priority;
 
 /**
@@ -10,11 +11,12 @@ import org.jboss.weld.experimental.Priority;
  * @author Michael Remijan mjremijan@yahoo.com @mjremijan
  */
 public class JarEntryFilter {
+
     @Inject
     protected Logger log;
 
     protected void filterJarEntries(
-        @Observes @Priority(JarEntryFinderEvent.FILTER) JarEntryFinderEvent event
+        @Observes @Priority(FILTER) JarEntryFinderEvent event
     ) {
         log.info("ENTER");
         event.setJarEntries(
